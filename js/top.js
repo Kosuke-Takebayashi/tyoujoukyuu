98b26e393713b68abce7f848b69de0c59433da48
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2025-07-07 18:52:05 +0900

    $を削除

--- js/top.js
+++ js/top.js
@@ -1,14 +1,14 @@
-const $topHeader = document.getElementById('top-header');
-const $topHeaderNav = document.getElementById('top-header__nav');
+const topHeader = document.getElementById('top-header');
+const topHeaderNav = document.getElementById('top-header__nav');
 
 
 window.addEventListener('scroll',function(){
     let scroll = document.documentElement.scrollTop;
     if (100 < scroll) {
-        $topHeader.classList.add('scroll');
-        $topHeaderNav.classList.add('scroll');
+        topHeader.classList.add('scroll');
+        topHeaderNav.classList.add('scroll');
     }else{
-        $topHeader.classList.remove('scroll');
-        $topHeaderNav.classList.remove('scroll');
+        topHeader.classList.remove('scroll');
+        topHeaderNav.classList.remove('scroll');
     }
 })
